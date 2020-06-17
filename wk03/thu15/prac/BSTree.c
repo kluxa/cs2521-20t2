8524637ee9135af2ca24c54da2f810e06ac97cf7
Kevin Luxa <k.luxa@unsw.edu.au>
2020-06-17 17:48:31 +1000

    weed 3 starter

---
+++ wk03/thu15/prac/BSTree.c
@@ -0,0 +1,38 @@
+
+#include <stdlib.h>
+
+#include "BSTree.h"
+
+/**
+ * Returns the number of nodes in the tree.
+ */
+int BSTreeNumNodes(BSTree t) {
+    return 0;
+}
+
+/**
+ * Returns  the  height  of  the given tree. The height of a tree is the
+ * number  of  edges  on  the  longest path from the root node to a leaf
+ * node. The height of an empty tree is -1.
+ */
+int BSTreeHeight(BSTree t) {
+    return -1;
+}
+
+/**
+ * Returns the number of internal nodes in the tree. An internal node is
+ * a  node  that  has  one  or more children, i.e., a node that is not a
+ * leaf.
+ */
+int countInternal(BSTree t) {
+    return 0;
+}
+
+/**
+ * Returns the depth of the node containing the given key in the tree if
+ * it exists, or -1 otherwise.
+ */
+int nodeDepth(BSTree t, int key) {
+    return -1;
+}
+
