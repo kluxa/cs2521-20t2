b91f45e00f9afcf12c4667abc0147a254941fe51
Kevin Luxa <k.luxa@unsw.edu.au>
2020-07-02 18:04:15 +1000

    week 5 thursday

--- wk05/thu15/q7/reachable.c
+++ wk05/thu15/q7/reachable.c
@@ -15,9 +15,21 @@ Set reachable(Graph g, Vertex v) {
 	return s;
 }
 
-// DFS
+// DFS - recursive!!!
 void doReachable(Graph g, Vertex v, Set s, int *visited) {
 	// visit v
 	// add it to the reachable set
+	visited[v] = 1;
+	SetAdd(s, v);
+
 	// then see if there's a neighbour we can visit
+	for (Vertex w = 0; w < GraphNumVertices(g); w++) {
+		if (!GraphIsAdjacent(g, v, w)) {
+			continue;
+		}
+		if (!visited[w]) {
+			// Do DFS on w
+			doReachable(g, w, s, visited);
+		}
+	}
 }
