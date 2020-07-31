9a6f1c18a6f99f407c34ed6d06fe3e44a07b86b2
Kevin Luxa <k.luxa@unsw.edu.au>
2020-08-01 05:37:18 +1000

    week 9

---
+++ wk09/q3/isStableSort.c
@@ -0,0 +1,40 @@
+
+#include <stdbool.h>
+
+#include "arrayUtil.h"
+#include "isStableSort.h"
+#include "Item.h"
+
+static void bubbleSort(Item A[], int size);
+static void swap(Item A[], int i, int j);
+
+bool isStableSort(Item original[], Item sorted[], int size) {
+	Item copy[size];
+	arrayCopy(copy, original, size);
+	bubbleSort(copy, size);
+	bool result = arraysEqual(copy, sorted, size);
+	return result;
+}
+
+// Known to be stable
+static void bubbleSort(Item A[], int size) {
+	bool swapped;
+	for (int i = size - 1; i > 0; i--) {
+		swapped = false;
+		for (int j = 0; j < i; j++) {
+			if (A[j].a > A[j + 1].a) {
+				swap(A, j, j + 1);
+				swapped = true;
+			}
+		}
+		if (swapped == false) {
+			break;
+		}
+	}
+}
+
+static void swap(Item A[], int i, int j) {
+	Item temp = A[i];
+	A[i] = A[j];
+	A[j] = temp;
+}
